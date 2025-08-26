6f7d6e766d4f1fa893d0533bcb39d276fadfc33a
Maithe Romagnoli <matheuszothavio@gmail.com>
2025-08-26 09:42:28 -0300

    MODIFY: correçã geral de erros

--- tests/test_juros_compostos.py
+++ tests/test_juros_compostos.py
@@ -1,7 +1,7 @@
 import pytest
 from juros.juros_composto import calcular_juros_compostos
 
-@pytest.mark.parametrize("capital, juros, tempo, situacao_esperada"
+@pytest.mark.parametrize("capital, juros, tempo, situacao_esperada",
                          [
                              (1000, 20, 1, (200, 1200)) #juros e montante
                          ]
@@ -29,22 +29,22 @@ def test_verifica_negativo():
 
 
 def test_verifica_string():
-    with pytest.raises(ValueError, match="O capital investido deve ser um número (int ou float)."):
+    with pytest.raises(ValueError, match="O capital investido deve ser um número \(int ou float\)."):
         calcular_juros_compostos("str", 20, 1)
 
-    with pytest.raises(ValueError, match="A taxa de juros deve ser um número (int ou float)."):
+    with pytest.raises(ValueError, match="A taxa de juros deve ser um número \(int ou float\)."):
         calcular_juros_compostos(1000, "str", 1)
 
-    with pytest.raises(ValueError, match="O tempo deve ser um número (int ou float)."):
+    with pytest.raises(ValueError, match="O tempo deve ser um número \(int ou float\)."):
         calcular_juros_compostos(1000, 20, "str")
 
 # verifica parametros vazios
 def test_verifica_valor_vazio():
-    with pytest.raises(ValueError, match="O capital investido deve ser um número (int ou float)."):
-        calcular_juros_compostos([], 20, 1)
+    with pytest.raises(ValueError, match="O capital investido deve ser um número \(int ou float\)."):
+        calcular_juros_compostos( 20, 1)
 
-    with pytest.raises(ValueError, match="O capital investido deve ser um número (int ou float)."):
-        calcular_juros_compostos(1000, [], 1)
+    with pytest.raises(ValueError, match="O capital investido deve ser um número \(int ou float\)."):
+        calcular_juros_compostos(1000, 1)
 
-    with pytest.raises(ValueError, match="O capital investido deve ser um número (int ou float)."):
-        calcular_juros_compostos(1000, 20, [])
+    with pytest.raises(ValueError, match="O capital investido deve ser um número \(int ou float\)."):
+        calcular_juros_compostos(1000, 20)
